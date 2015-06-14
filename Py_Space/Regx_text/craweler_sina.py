af247dc5dbdc9d27ca5e62a088a36ffab3fbe927
cohbird <cohbird.zn@gmail.com>
2015-06-15 06:08:32 +0800

    UpdateMySpace

---
+++ Py_Space/Regx_text/craweler_sina.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import re
+import requests
+
+html = open('/home/cohbird/Desktop/yuhonghong.html', 'r')
+content = html.read()
+html.close()
+
+fl = re.findall('width="...px" height="...px" src="(http://ww..sinaimg.cn/cmw218/.................................jpg)">', content, re.S)
+i = 1;
+for each in fl:
+    print 'Downloading:(%d)' % (i) + each
+    pic = requests.get(str(each))
+    fp = open('余烘烘' + str(i) + '.jpg', 'wb')
+    fp.write(pic.content)
+    fp.close()
+    i += 1
